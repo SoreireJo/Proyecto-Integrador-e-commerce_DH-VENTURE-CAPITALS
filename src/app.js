38d5429d1be2d5532675e19f5fd984c61998026c
Nerea <nchanampe@gmail.com>
2022-02-17 15:36:28 -0300

    Se desarrollo HTML y CSS de formulario LOGIN

--- src/app.js
+++ src/app.js
@@ -14,3 +14,11 @@ app.get("/", (req, res) => {
     // Va a la Home
     res.sendFile(path.join(__dirname, "./views/web/home.html"));
 });
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/web/login.html"));
+  });
+  
+  app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/register.html"));
+  });
