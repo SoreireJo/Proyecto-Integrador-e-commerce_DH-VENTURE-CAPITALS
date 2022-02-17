bf4d9f88d47db2bec513385829f321be51f8099b
SoreireJo <soreirejoseluis@gmail.com>
2022-02-17 20:35:22 -0300

    * Actualización de home a indez.html, * Primera vista de productos en index, * Actualización de app.js

--- src/app.js
+++ src/app.js
@@ -12,5 +12,25 @@ app.listen(3000, () => {
 
 app.get("/", (req, res) => {
     // Va a la Home
-    res.sendFile(path.join(__dirname, "./views/web/home.html"));
+    res.sendFile(path.join(__dirname, "./views/web/index.html"));
+});
+
+app.get("/login", (req, res) => {
+    // Login
+    res.sendFile(path.join(__dirname, "./views/web/login.html"));
+});
+
+app.get("/register", (req, res) => {
+    // Registro
+    res.sendFile(path.join(__dirname, "./views/web/register.html"));
+});
+
+app.get("/product", (req, res) => {
+    // Detalle de Producto
+    res.sendFile(path.join(__dirname, "./views/web/productDetail.html"));
+});
+
+app.get("/product_cart", (req, res) => {
+    // Carrito de Compras
+    res.sendFile(path.join(__dirname, "./views/web/productCart.html"));
 });
