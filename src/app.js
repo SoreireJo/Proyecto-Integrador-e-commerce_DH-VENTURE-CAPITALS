7b6d56bf21d481db558d55b01a8a66fe623ec6f8
Nerea <nchanampe@gmail.com>
2022-02-18 00:01:46 -0300

    Se desarrollo HTML Y CSS del formulario REGISTER

--- src/app.js
+++ src/app.js
@@ -14,3 +14,7 @@ app.get("/", (req, res) => {
     // Va a la Home
     res.sendFile(path.join(__dirname, "./views/web/home.html"));
 });
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/web/register.html"));
+  });
