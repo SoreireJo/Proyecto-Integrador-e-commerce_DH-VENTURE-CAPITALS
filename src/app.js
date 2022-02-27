31512d039d3735d7f5597a6ac1b9eacfd3c321f2
sigridvg <sigridvg96@gmail.com>
2022-02-27 03:21:33 -0300

    detail products

--- src/app.js
+++ src/app.js
@@ -14,3 +14,8 @@ app.get("/", (req, res) => {
     // Va a la Home
     res.sendFile(path.join(__dirname, "./views/web/home.html"));
 });
+
+app.get("/product-detail", (req, res) => {
+    // Va a la Home
+    res.sendFile(path.join(__dirname, "./views/product/product_detail.html"));
+});
