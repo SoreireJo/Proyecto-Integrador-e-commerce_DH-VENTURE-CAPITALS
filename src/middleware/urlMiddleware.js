8baf6f09ec775ff1dad65b6f3da1f3f83cb185e8
SoreireJo <soreirejoseluis@gmail.com>
2022-03-22 14:24:12 -0300

    (Spring 4): * Fix rutas, * Fix middleware, * Se cargaron 2 productos nuevos por formulario, * Cambios menores

--- src/middleware/urlMiddleware.js
+++ src/middleware/urlMiddleware.js
@@ -1,11 +1,12 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
 
-const pahtLog = path.join(__dirname, '../userLogs.txt')
+const pahtLog = path.join(__dirname, '../userLogs.txt');
 
 
 function userLog(req, res, next){
     fs.appendFileSync(pahtLog, 'El usuario ingreso a: '+ req.url + "\n")
+    next();
 }
 
 module.exports= userLog;
